60f30c8d4043536e8a266780764cc86c9c82b76a
RamiroAlvaradoD <ramiro.alvarado@alumnos.ucn.cl>
2025-11-18 22:29:01 -0300

    Creada la clase Tarea y sus clases hijas
    Se creo la clase Abstracta Tarea, y sus clases hijas, que heredan sus caracteristicas y metodos, y se dejo comentado lo siguiente en que se debe trabajar (Clases del patron visitor)

--- src/taller/Tarea.java
+++ src/taller/Tarea.java
@@ -1,5 +1,55 @@
 package taller;
+import visitor.TareaVisitor;
 
 public abstract class Tarea {
+	protected String proyectoId;
+	protected String tareaId;
+	protected String descripcion;
+	protected String estado;
+	protected String responsable;
+	protected String complejidad;
+	protected String fecha;
+
+	public Tarea(String proyectoId, String tareaId, String descripcion, String estado, String responsable,
+			String complejidad, String fecha) {
+		
+		this.proyectoId = proyectoId;
+		this.tareaId = tareaId;
+		this.descripcion = descripcion;
+		this.estado = estado;
+		this.responsable = responsable;
+		this.complejidad = complejidad;
+		this.fecha = fecha;
+	}
+
+	public String getProyectoId() {
+		return proyectoId;
+	}
+
+	public String getTareaId() {
+		return tareaId;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public String getResponsable() {
+		return responsable;
+	}
+
+	public String getComplejidad() {
+		return complejidad;
+	}
+
+	public String getFecha() {
+		return fecha;
+	}
+	
+	public abstract void aceptar(TareaVisitor visitor);
 
 }
