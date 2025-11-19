5f18f2cb474a967660d1a4f63d7758f377e6299b
RamiroAlvaradoD <ramiro.alvarado@alumnos.ucn.cl>
2025-11-18 21:06:12 -0300

    Iniciadas las clases de Strategy
    se declaro la interface del patron Strategy y se implemento el metodo, aun sin logica, para las clases que implementan PrioridadStrategy

--- src/strategy/PrioridadComplejidad.java
+++ src/strategy/PrioridadComplejidad.java
@@ -1,5 +1,15 @@
 package strategy;
 
+import java.util.ArrayList;
+
+import taller.Tarea;
+
 public class PrioridadComplejidad implements PrioridadStrategy {
+
+	@Override
+	public void ordenar(ArrayList<Tarea> lista) {
+		//Se dejo escrito el metodo que establecimos en la interface, completar mas rato.
+		
+	}
 
 }
--- src/strategy/PrioridadFecha.java
+++ src/strategy/PrioridadFecha.java
@@ -1,5 +1,13 @@
 package strategy;
+import taller.Tarea;
+import java.util.ArrayList;
 
 public class PrioridadFecha implements PrioridadStrategy {
+	
+	@Override 
+	public void ordenar(ArrayList <Tarea> lista) {
+		//Se dejo escrito el metodo que establecimos en la interface, completar mas rato.
+		
+	}
 
 }
