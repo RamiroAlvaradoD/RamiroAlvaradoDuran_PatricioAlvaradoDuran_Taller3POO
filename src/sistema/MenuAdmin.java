05c5dd6ecca2f3e6ae6eb0b63f8b017e03a28279
Patricio Alvarado <Garaxlight@users.noreply.github.com>
2025-11-20 01:59:30 -0300

    Logica de Menu Admin creado
    Se añadio la logica para el menu de administrador, con los metodos para cada una de las opciones del menú

--- src/sistema/MenuAdmin.java
+++ src/sistema/MenuAdmin.java
@@ -1,16 +1,144 @@
 package sistema;
+
+import java.util.Scanner;
+
+import javax.swing.RepaintManager;
+
+import factory.TareaFactoryImpl;
+import strategy.PrioridadComplejidad;
+import strategy.PrioridadFecha;
+import strategy.PrioridadStrategy;
+import strategy.PrioridadTipo;
 import taller.Administrador;
+import taller.Proyecto;
+import taller.Tarea;
 
 public class MenuAdmin {
-	
+
 	private Administrador admin;
-	
+
 	public MenuAdmin(Administrador Admin) {
 		this.admin = admin;
 	}
 
 	public void mostrarOpciones() {
-		System.out.println("Menu en desarrollo.");		
-	}
+		Scanner sc = new Scanner(System.in);
+		int opcion;
+		do {
+			System.out.println("\n--- MENÚ ADMINISTRADOR ---");
+			System.out.println("1. Ver Proyectos");
+			System.out.println("2. Agregar proyecto");
+			System.out.println("3. Eliminar proyecto");
+			System.out.println("4. Ver tareas de un proyecto");
+			System.out.println("5. Agregar tarea");
+			System.out.println("6. Eliminar tarea");
+			System.out.println("7. Ordenar tareas de un proyecto");
+			System.out.println("8. Generar reporte");
+			System.out.println("0. Salir");
+			System.out.print("Ingrese opcion: ");
+			opcion = sc.nextInt();
+			sc.nextLine();
+			
+			switch (opcion) {
+			case 1:
+				for (Proyecto p : Sistema.getInstancia().getProyectos()) {
+					System.out.println(p);
+				}
+				break;
+			case 2:
+				System.out.print("ID proyecto: ");
+				String idP = sc.nextLine();
+				System.out.print("Nombre: ");
+				String nombreP = sc.nextLine();
+				System.out.print("Responsable: ");
+				String respP = sc.nextLine();
+				Proyecto nuevoP = new Proyecto(idP, nombreP, respP);
+				admin.agregarProyecto(nuevoP);
+				break;
+			case 3:
+				System.out.print("ID del proyecto a eliminar: ");
+				String idEliminar = sc.nextLine();
+				admin.eliminarProyecto(idEliminar);
+				break;
+			case 4:
+				System.out.print("ID proyecto: ");
+				String idVer= sc.nextLine();
+				Proyecto proy = Sistema.getInstancia().buscarProyecto(idVer);
+				if (proy !=null) {
+					for (Tarea t: proy.getTareas()) {
+						System.out.println(t.getDescripcion());
+					}
+				}
+				break;
+			case 5:
+				System.out.print("ID del proyecto al que pertenece la tarea: ");
+				String proyectoID = sc.nextLine();
+				
+				Proyecto proyecto =Sistema.getInstancia().buscarProyecto(proyectoID);
+				if (proyecto == null) {
+					System.out.println("Error: El proyecto no existe.");
+					break;
+				}
+				System.out.print("ID de la tarea: ");
+				String tareaID = sc.nextLine();
+				
+				System.out.print("Tipo de tarea (Bug/Feature/Documentacion): ");
+				String tipo = sc.nextLine();
+				
+				System.out.print("Descripcion: ");
+				String descripcion = sc.nextLine();
+				
+				System.out.print("Estado inicial (Pendiente/En Proceso/Terminado): ");
+				String estado = sc.nextLine();
+				
+				System.out.print("Responsable: ");
+				String responsable= sc.nextLine();
+				
+				System.out.print("Complejidad (Alta/Media/Baja): ");
+				String complejidad = sc.nextLine();
+				
+				System.out.print("Fecha (YYYY/MM/DD): ");
+				String fecha = sc.nextLine();
+				
+				TareaFactoryImpl factory = new TareaFactoryImpl();
+				
+				try {
+					Tarea nuevaTarea = factory.crearTarea(tipo, proyectoID, tareaID, descripcion,
+							estado, responsable, complejidad, fecha);
+					admin.agregarTarea(nuevaTarea);
+					proyecto.addTarea(nuevaTarea);
+					
+					System.out.println("Tarea agregada exitosamente!");
+					
+				} catch (IllegalArgumentException e) {
+					System.out.println("Error: "+e.getMessage());
+				}
+				break;
+			case 6:
+				System.out.print("ID tarea a eliminar: ");
+				String tID = sc.nextLine();
+				admin.eliminarTarea(tID);
+				break;
+			case 7:
+				System.out.print("ID proyecto: ");
+				String pID = sc.nextLine();
+				Proyecto pr = Sistema.getInstancia().buscarProyecto(pID);
+				if (pr != null) {
+					System.out.println("Ordenar por: 1. Fecha 2. Complejidad 3. Tipo");
+					int tipoOrden = sc.nextInt();
+					sc.nextLine();
+					PrioridadStrategy est = null;
+					if (tipoOrden == 1) est = new PrioridadFecha();
+					else if (tipoOrden == 2) est = new PrioridadComplejidad();
+					else est = new PrioridadTipo();
+					admin.asignarPrioridad(pr, est);
+				}
+				break;
+			case 8:
+				admin.generarReporte();
+				break;
+			}
+		} while (opcion != 0);
 
+	}
 }
