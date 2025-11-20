232beb5ac29dea5b3f99578a2bf23d5ff63f63e2
Patricio Alvarado <Garaxlight@users.noreply.github.com>
2025-11-20 01:26:45 -0300

    Adicion de metodo CrearTarea
    Se hace la implementacion del metodo Crear tarea para la adicion de nuevas subclases de tarea

--- src/factory/TareaFactoryImpl.java
+++ src/factory/TareaFactoryImpl.java
@@ -1,5 +1,23 @@
 package factory;
 
+import taller.Tarea;
+import taller.Bug;
+import taller.Feature;
+import taller.Documentacion;
+
 public class TareaFactoryImpl implements TareaFactory {
 
+	public Tarea crearTarea(String tipo, String proyectoID, String tareaID, String descripcion,
+			String estado, String responsable, String complejidad, String fecha) {
+		switch (tipo.toLowerCase()) {
+		case "bug": return new Bug(proyectoID, tareaID, descripcion, estado, responsable, complejidad, fecha);
+		
+		case "feature": return new Feature(proyectoID, tareaID, descripcion, estado, responsable, complejidad, fecha);
+		
+		case "documentacion": return new Documentacion(proyectoID, tareaID, descripcion, estado,
+				responsable, complejidad, fecha);
+		default: throw new IllegalArgumentException("Tipo de tarea Invalido");
+		}
+		
+	}
 }
